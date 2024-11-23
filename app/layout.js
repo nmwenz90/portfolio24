050336c67a44663c46a437d9bb994801d2752372
Nate <kings_el@yahoo.com>
2024-11-22 18:02:16 -0700

    fix css path

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-import "@css/bootstrap.min.css";
-import "@css/coloring.css";
-import "@css/plugins.css";
-import "@css/style.css";
+import "../public/css/bootstrap.min.css";
+import "../public/css/coloring.css";
+import "../public/css/coloring.css";
+import "../public/css/plugins.css";
+import "../public/css/style.css";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import "./globals.css";
